14f36fc546f08972e1808367e05b4efc5b33b7c5
Jeffery Abbott <jeff@jefferyabbott.com>
2021-06-07 18:59:13 -0700

    Update comments to follow Udacity style guide

--- app.js
+++ app.js
@@ -72,11 +72,16 @@ const dinoData = [
         "when": "Holocene",
         "fact": "All birds are living dinosaurs."
     }
-]
+];
 
 
 
-// Dino Constructor
+/**
+* @description Represents a Dinosaur object
+* @constructor
+* @param {string} dino - Dinosaur data
+* @param {string} image - URL of the dinosaur image
+*/
 class Dino {
     constructor(dino, image) {
         this.species = dino.species
@@ -89,132 +94,145 @@ class Dino {
         this.image = image
     }
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    /**
+    * @description compares the diet of a dino and the human
+    * @param {human} human - the human object to compare against the dino
+    * @returns {string} - text comparing the human and dino diets
+    */
     compareDiet(human) {
-        return this.diet === human.diet ? `${this.species} and you are both ${this.diet}s.` : `${this.species} has a different diet than you.`
+        return this.diet === human.diet ? `${this.species} and you are both ${this.diet}s.` : `${this.species} has a different diet than you.`;
     }
     
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+    /**
+    * @description compares the height of a dino and the human
+    * @param {human} human - the human object to compare against the dino
+    * @returns {string} - text comparing the human and dino height
+    */
     compareHeight(human) {
         if (this.height > human.height) {
-            return `${this.species} is taller than you.`
+            return `${this.species} is taller than you.`;
         } else if (this.height < human.height) {
-            return `${this.species} is shorter than you.`
+            return `${this.species} is shorter than you.`;
         } else {
-            return `You and ${this.species} are the same height.`
+            return `You and ${this.species} are the same height.`;
         }
     }
-   
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+    /**
+    * @description compares the weight of a dino and the human
+    * @param {human} human - the human object to compare against the dino
+    * @returns {string} - text comparing the human and dino weight
+    */
     compareWeight(human) {
-        const weightDifference = Math.abs(this.weight - human.weight)
+        const weightDifference = Math.abs(this.weight - human.weight);
         if (this.weight > human.weight) {
-            return `${this.species} is ${weightDifference} lbs heavier than you.`
+            return `${this.species} is ${weightDifference} lbs heavier than you.`;
         } else if (this.weight < human.weight) {
-            return `You are ${weightDifference} lbs heavier than ${this.species}.`
+            return `You are ${weightDifference} lbs heavier than ${this.species}.`;
         } else {
-            return `You and ${this.species} are the same weight.`
+            return `You and ${this.species} are the same weight.`;
         }
     }
 
 }
 
 
+
 // Create Dino Objects
-let dinos = dinoData.map(d => new Dino(d, `/images/${d.species.toLowerCase()}.png`))
+let dinos = dinoData.map(d => new Dino(d, `/images/${d.species.toLowerCase()}.png`));
 
 
-// randomize the order of the dino array
-// https://stackoverflow.com/questions/37165469/print-array-in-random-order
+
+/**
+* @description randomize the order of the dino array
+* @source https://stackoverflow.com/questions/37165469/print-array-in-random-order
+*/
 dinos.sort(function() {
-    return .5 - Math.random()
+    return .5 - Math.random();
 })
 
 
 
 // Create Human Object
 function Human(name, heightFt, heightIn, weight, diet, image) {
-    this.name = name
-    this.height = (heightFt * 12) + heightIn
-    this.weight = weight
-    this.diet = diet
-    this.image = image
+    this.name = name;
+    this.height = (heightFt * 12) + heightIn;
+    this.weight = weight;
+    this.diet = diet;
+    this.image = image;
 }
 
 
 
 function compareUserToDino() {
-
     // Use IIFE to get human data from form
     const human = (function createHumanObject(){
-        const name = document.querySelector("#name").value
-        const heightFt = document.querySelector("#feet").value
-        const heightIn = document.querySelector("#inches").value
-        const weight = document.querySelector("#weight").value
-        const diet = document.querySelector("#diet").value
-        return new Human(name, heightFt, heightIn, weight, diet, '/images/human.png')
-    })()
-
+        const name = document.querySelector("#name").value;
+        const heightFt = document.querySelector("#feet").value;
+        const heightIn = document.querySelector("#inches").value;
+        const weight = document.querySelector("#weight").value;
+        const diet = document.querySelector("#diet").value;
+        return new Human(name, heightFt, heightIn, weight, diet, '/images/human.png');
+    })();
 
     // Generate Tiles for each Dino in Array
-    const grid = document.querySelector('#grid')
+    const grid = document.querySelector('#grid');
     for (dino of dinos) {
         
         // set species
-        const gridItemTitle = document.createElement('h2')
-        gridItemTitle.textContent = dino.species
+        const gridItemTitle = document.createElement('h2');
+        gridItemTitle.textContent = dino.species;
 
         // set image
-        const gridItemImg = document.createElement('img')
-        gridItemImg.src = `images/${dino.species}.png`
+        const gridItemImg = document.createElement('img');
+        gridItemImg.src = `images/${dino.species}.png`;
 
         // set fact
-        const gridItemFact = document.createElement('p')
+        const gridItemFact = document.createElement('p');
         if (dino.species === 'Pigeon') {
-            gridItemFact.textContent = dino.fact
+            gridItemFact.textContent = dino.fact;
         } else {
-            const facts = [dino.fact, dino.compareDiet(human), dino.compareWeight(human), dino.compareHeight(human)]
+            const facts = [dino.fact, dino.compareDiet(human), dino.compareWeight(human), dino.compareHeight(human)];
             // get random item from an array
             // https://www.w3resource.com/javascript-exercises/javascript-array-exercise-35.php
-            gridItemFact.textContent = facts[Math.floor(Math.random()*facts.length)]
+            gridItemFact.textContent = facts[Math.floor(Math.random()*facts.length)];
         }
         
         // assemble tile
-        const gridItem = document.createElement('div')
-        gridItem.className = 'grid-item'
-        gridItem.appendChild(gridItemTitle)
-        gridItem.appendChild(gridItemImg)
-        gridItem.appendChild(gridItemFact)
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        gridItem.appendChild(gridItemTitle);
+        gridItem.appendChild(gridItemImg);
+        gridItem.appendChild(gridItemFact);
         
         // Add tiles to DOM
-        grid.appendChild(gridItem)
+        grid.appendChild(gridItem);
     }
 
+
     // add human to center tile
-    const gridItemTitle = document.createElement('h2')
-    gridItemTitle.innerHTML = human.name
+    const gridItemTitle = document.createElement('h2');
+    gridItemTitle.innerHTML = human.name;
 
-    const gridItemImg = document.createElement('img')
-    gridItemImg.src = human.image
+    const gridItemImg = document.createElement('img');
+    gridItemImg.src = human.image;
 
-    const gridItem = document.createElement('div')
-    gridItem.className = 'grid-item'
-    gridItem.appendChild(gridItemTitle)
-    gridItem.appendChild(gridItemImg)
+    const gridItem = document.createElement('div');
+    gridItem.className = 'grid-item';
+    gridItem.appendChild(gridItemTitle);
+    gridItem.appendChild(gridItemImg);
   
     // place human in center of grid
-    grid.insertBefore(gridItem, grid.children[grid.children.length / 2])
+    grid.insertBefore(gridItem, grid.children[grid.children.length / 2]);
         
     // Remove form from screen
-    document.querySelector('#dino-compare').style.display = 'none'
+    document.querySelector('#dino-compare').style.display = 'none';
 }
 
 
+
 // On button click, prepare and display infographic
-document.querySelector('#btn').addEventListener('click', compareUserToDino)
+document.querySelector('#btn').addEventListener('click', compareUserToDino);
 
 
 
